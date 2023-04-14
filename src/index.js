4e97ccdde8f71818d9b2606dadd5043e6ae9c3e2
Eric <ericleee777@gmail.com>
2023-04-14 01:24:26 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import ReactGlobe from "react-globe";
 
 function App() {
-  return <ReactGlobe height="100vh" width="100vw" />;
+  return <ReactGlobe height="100vh" width="100vw  " />;
 }
 
 const rootElement = document.getElementById("root");
